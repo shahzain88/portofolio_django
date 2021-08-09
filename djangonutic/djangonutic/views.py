5913fe168c6220e73198dba36f34183f70568fe4
shahzain88 <itit080itit@gmail.com>
2021-08-09 12:45:49 +0900

    first upload

---
+++ djangonutic/djangonutic/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    # return HttpResponse("Home or / ")
+    return render(request, "homepage.html")
+
+
+def about(request):
+    # return HttpResponse('About')
+    return render(request, "about.html")
